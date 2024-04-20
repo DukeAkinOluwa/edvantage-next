2c2594bcb14b83144deb5016e998588ad8681920
DukeAkinOluwa <akindukoakinoluwa@gmail.com>
2024-04-20 11:29:59 +0100

    New: Adds the FAQs and all its features and functionalities

--- src/app/FAQs/page.jsx
+++ src/app/FAQs/page.jsx
@@ -1,6 +1,36 @@
-export default function FAQs() {
-    return (
-      <>FAQs</>
-    );
+'use client'
+
+import SideMenu from "@/components/SideMenu"
+import PageRightHeader from "@/components/PageRightHeader"
+import faq_info from "@/DB/faqdata.json"
+
+export default function FAQs(props){
+    
+    const faqinfo = faq_info.data
+
+    return(
+        <>
+        <PageRightHeader page_title={props.page_title} userlevel="23"/>
+        <div className="faq-table table">
+            <div className="faq-header row">
+                <div className="column1"><h5>S/N</h5></div>
+                <div className="column2"><h5>Question</h5></div>
+                <div className="column3"><h5>Answer</h5></div>
+            </div>
+            {faqinfo.map((faqs, index) => (
+                <FAQTemplate key={index} data={faqs} />
+            ))}
+        </div>
+        </>
+    )
+    function FAQTemplate(props){
+      const data = props.data
+      return(
+          <div className="faq-content row">
+              <div className="column column1"><h3 className="pc-hidden">S/N</h3><p>{data.sn}</p></div>
+              <div className="column column2"><h3 className="pc-hidden">Question</h3><p>{data.question}</p></div>
+              <div className="column column3"><h3 className="pc-hidden">Answer</h3><p>{data.answer}</p></div>
+          </div>
+      )
   }
-  
+}
