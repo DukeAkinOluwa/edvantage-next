88a7f6fa3c7a4a7d4fd31b8799964cd88b58bd90
DukeAkinOluwa <akindukoakinoluwa@gmail.com>
2024-07-18 22:36:40 +0100

    Update: Starts working on the progress page

--- src/app/Progress/page.jsx
+++ src/app/Progress/page.jsx
@@ -1,5 +1,31 @@
 export default function Progress() {
   return (
-    <>Progress</>
+    <div className="progress-page">
+      <div className="category">
+        <div className="header">
+          <h3>Points</h3>
+        </div>
+      </div>
+      <div className="category">
+        <div className="header">
+          <h3>Level</h3>
+        </div>
+      </div>
+      <div className="category">
+        <div className="header">
+          <h3>Quests</h3>
+        </div>
+      </div>
+      <div className="category">
+        <div className="header">
+          <h3>Trophies</h3>
+        </div>
+      </div>
+      <div className="category">
+        <div className="header">
+          <h3>Badges</h3>
+        </div>
+      </div>
+    </div>
   );
 }
